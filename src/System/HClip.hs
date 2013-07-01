ee670a0fadd74c47247c34a2ee17a3bd1f57ffcc
unknown <thomas@Thomas-W7.dodav.int.odav.de>
2013-07-01 16:27:27 +0200

    Changed to Typeclass Approach

--- src/System/HClip.hs
+++ src/System/HClip.hs
@@ -1,7 +1,6 @@
 
 module System.HClip (getClipboard, setClipboard, modifyClipboard) where
 
-
 import System.Process
 import System.Info (os)
 import System.IO (Handle, hPutStr, hClose)
@@ -11,82 +10,85 @@ import Control.Exception
 import System.IO.Strict (hGetContents) -- see http://hackage.haskell.org/package/strict
 import System.Exit
 import Control.Monad.Error
+import Data.List
 
 
 type ErrorWithIO = ErrorT String IO
-type IOHandles = (Handle, Handle)
-type OSType = String
 
-data CommandType = GetClipboard | SetClipboard String
+data Command = GetClipboard | SetClipboard String 
 
 
-stdin, stdout :: IOHandles -> Handle
-stdin = fst
-stdout = snd
+data Linux = Linux deriving (Show)
+data Darwin = Darwin deriving (Show)
+data Windows = Windows deriving (Show)
 
 
-externalCommand "darwin" GetClipboard = "pbcopy"
-externalCommand "darwin" (SetClipboard _) = "pbpaste"
+class SupportedOS a where
+  clipboard :: a -> Command -> IO (Either String String)
+    
 
-externalCommand "xsel" GetClipboard = "xsel -o"
-externalCommand "xsel" (SetClipboard _) = "xsel -i"
+instance SupportedOS Darwin where
+  clipboard Darwin GetClipboard = runErrorT $ withExternalCommand "pbcopy" GetClipboard    
+  clipboard Darwin c@(SetClipboard s) = runErrorT $ withExternalCommand "pbpaste" c
 
-externalCommand "xclip" GetClipboard = "xclip -selection c -o"
-externalCommand "xclip" (SetClipboard _) = "xclip -selection c"
+
+instance SupportedOS Linux where
+  clipboard Linux command = runErrorT $ do
+    prog <- chooseFirstCommand ["xsel", "xclip"]
+    withExternalCommand (decode prog command) command
+    where
+      decode "xsel" GetClipboard = "xsel -o"
+      decode "xsel" (SetClipboard _) = "xsel -i"
+      decode "xclip" GetClipboard = "xclip -selection c -o"
+      decode "xclip" (SetClipboard _) = "xclip -selection c"
+    
+
+instance SupportedOS Windows where
+  clipboard Windows GetClipboard = undefined
+  clipboard Windows (SetClipboard s) = undefined
+
+
+withExternalCommand :: String -> Command -> ErrorWithIO String
+withExternalCommand prog command = 
+  liftIO $ bracket (runInteractiveCommand prog)
+                   (\(inp,outp,stderr,_) -> mapM_ hClose [inp,outp,stderr])
+                   (\(inp,outp,_,_) -> (action command) (inp, outp))
+  where
+    action GetClipboard = hGetContents . stdout
+    action (SetClipboard text) = (flip hPutStr text >=> const (return text)) . stdin
+    stdin = fst
+    stdout = snd
+
+
+chooseFirstCommand :: [String] -> ErrorWithIO String
+chooseFirstCommand cmds = do
+  results <- liftIO $ mapM whichCommand cmds
+  maybe (throwError $ "HClip requires " ++ apps ++ " installed.")
+        return
+        (getFirst (mconcat $ map First results))
+  where apps = intercalate " or " cmds
 
 
 whichCommand :: String -> IO (Maybe String)
-whichCommand cmd = do 
-  (exitCode,_,_) <- readProcessWithExitCode "which" [cmd] "" 
+whichCommand cmd = do
+  (exitCode,_,_) <- readProcessWithExitCode "which" [cmd] ""
   case exitCode of
     ExitSuccess -> return $ Just cmd
     ExitFailure _ -> return Nothing
 
 
-getLinuxCommand :: ErrorWithIO String
-getLinuxCommand = do
-  results <- liftIO $ mapM whichCommand ["xsel", "xclip"]
-  maybe (throwError "HClip requires xclip or xsel installed.")
-        return
-        (getFirst (mconcat $ map First results))
-
-
-getExternalCommand :: OSType -> CommandType -> ErrorWithIO String
-getExternalCommand "linux" commandType = getLinuxCommand >>= return . flip externalCommand commandType
-getExternalCommand "darwin" commandType = return $ externalCommand "darwin" commandType
-getExternalCommand unknownOS _ = throwError ("Unsupported OS: " ++ unknownOS)
-
-
-withExternalCommand :: OSType -> CommandType -> IO (Either String String)
-withExternalCommand osType commandType = runErrorT $ do
-  cmd <- getExternalCommand osType commandType
-  liftIO $ bracket (runInteractiveCommand cmd)
-                   (\(inp,outp,stderr,_) -> mapM_ hClose [inp,outp,stderr])
-                   (\(inp,outp,_,_) -> (action commandType) (inp, outp))
-  where
-    action GetClipboard = hGetContents . stdout
-    action (SetClipboard text) = (flip hPutStr text >=> const (return text)) . stdin
-
-
-winCommand GetClipboard = undefined
-winCommand (SetClipboard _) = undefined
-
-
-execCommand :: CommandType -> IO (Either String String)
-execCommand command = 
-  case os of
-    "windows" -> winCommand command
-    osType -> withExternalCommand osType command
+dispatchCommand = case os of
+  "mingw32" -> clipboard Windows 
+  "linux" -> clipboard Linux
+  "darwin" -> clipboard Darwin
 
 
 getClipboard :: IO (Either String String)
-getClipboard = execCommand GetClipboard
-
+getClipboard = dispatchCommand GetClipboard
 
 setClipboard :: String -> IO (Either String String)
-setClipboard = execCommand . SetClipboard 
+setClipboard = dispatchCommand . SetClipboard
 
-
-modifyClipboard :: (String -> String) -> IO (Either String String) 
+modifyClipboard :: (String -> String) -> IO (Either String String)
 modifyClipboard = flip (liftM . liftM) getClipboard >=> either (return . Left) setClipboard
 
