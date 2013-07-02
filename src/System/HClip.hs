a71458575fc18c32e68a9eaecc38a79fca913fcb
unknown <thomas@Thomas-W7.dodav.int.odav.de>
2013-07-02 11:30:14 +0200

    Added File Header

--- src/System/HClip.hs
+++ src/System/HClip.hs
@@ -1,37 +1,77 @@
 
-module System.HClip (getClipboard, setClipboard, modifyClipboard) where
+--------------------------------------------------------------------
+-- |
+-- Module : System.HClip
+-- Copyright : (c) Jens Thomas
+-- License : BSD3
+--
+-- Maintainer: Jens Thomas <jetho@gmx.de>
+-- Stability : experimental
+-- Portability: 
+--
+-- A small cross-platform library for reading and modifying the 
+-- system clipboard. 
+-- 
+--------------------------------------------------------------------
 
-import System.Process
+module System.HClip (
+        getClipboard, 
+        setClipboard, 
+        modifyClipboard
+  ) where
+
+import System.Process (runInteractiveCommand, readProcessWithExitCode) 
 import System.Info (os)
 import System.IO (Handle, hPutStr, hClose)
-import Control.Monad
-import Data.Monoid
-import Control.Exception
+import Data.Monoid 
+import Control.Exception (bracket)
 import System.IO.Strict (hGetContents) -- see http://hackage.haskell.org/package/strict
-import System.Exit
+import System.Exit 
 import Control.Monad.Error
 import Data.List (intercalate)
 
 
 type ErrorWithIO = ErrorT String IO
 
+-- Clipboard Actions
 data Command = GetClipboard | SetClipboard String 
 
-
+-- Supported Operating Systems
 data Linux = Linux deriving (Show)
 data Darwin = Darwin deriving (Show)
 data Windows = Windows deriving (Show)
 
 
+-- type class for supported operating systems
 class SupportedOS a where
   clipboard :: a -> Command -> IO (Either String String)
-    
 
+-- read clipboard contents
+getClipboard :: IO (Either String String)
+getClipboard = dispatchCommand GetClipboard
+
+-- set clipboard contents
+setClipboard :: String -> IO (Either String String)
+setClipboard = dispatchCommand . SetClipboard
+
+-- apply function to clipboard and return the new contents
+modifyClipboard :: (String -> String) -> IO (Either String String)
+modifyClipboard = flip (liftM . liftM) getClipboard >=> either (return . Left) setClipboard
+
+-- select the supported operating system
+dispatchCommand :: Command -> IO (Either String String)
+dispatchCommand = case os of
+  "mingw32" -> clipboard Windows 
+  "linux" -> clipboard Linux
+  "darwin" -> clipboard Darwin
+  unknownOS -> const $ return . Left $ "Unsupported OS: " ++ unknownOS
+
+-- Mac OS    
 instance SupportedOS Darwin where
   clipboard Darwin GetClipboard = runErrorT $ withExternalCommand "pbcopy" GetClipboard    
   clipboard Darwin c@(SetClipboard s) = runErrorT $ withExternalCommand "pbpaste" c
 
-
+-- Linux support
 instance SupportedOS Linux where
   clipboard Linux command = runErrorT $ do
     prog <- chooseFirstCommand ["xsel", "xclip"]
@@ -42,12 +82,13 @@ instance SupportedOS Linux where
       decode "xclip" GetClipboard = "xclip -selection c -o"
       decode "xclip" (SetClipboard _) = "xclip -selection c"
     
-
+-- TODO: Windows support
 instance SupportedOS Windows where
   clipboard Windows GetClipboard = undefined
   clipboard Windows (SetClipboard s) = undefined
 
 
+-- run external command for accessing the system clipboard
 withExternalCommand :: String -> Command -> ErrorWithIO String
 withExternalCommand prog command = 
   liftIO $ bracket (runInteractiveCommand prog)
@@ -60,6 +101,7 @@ withExternalCommand prog command =
     stdout = snd
 
 
+-- search for installed programs and return the first match
 chooseFirstCommand :: [String] -> ErrorWithIO String
 chooseFirstCommand cmds = do
   results <- liftIO $ mapM whichCommand cmds
@@ -69,28 +111,11 @@ chooseFirstCommand cmds = do
   where apps = intercalate " or " cmds
 
 
+-- use the which-command to check if cmd is installed
 whichCommand :: String -> IO (Maybe String)
 whichCommand cmd = do
   (exitCode,_,_) <- readProcessWithExitCode "which" [cmd] ""
   case exitCode of
     ExitSuccess -> return $ Just cmd
     ExitFailure _ -> return Nothing
-
-
-dispatchCommand :: Command -> IO (Either String String)
-dispatchCommand = case os of
-  "mingw32" -> clipboard Windows 
-  "linux" -> clipboard Linux
-  "darwin" -> clipboard Darwin
-  unknownOS -> const $ return . Left $ "Unsupported OS: " ++ unknownOS
-
-
-getClipboard :: IO (Either String String)
-getClipboard = dispatchCommand GetClipboard
-
-setClipboard :: String -> IO (Either String String)
-setClipboard = dispatchCommand . SetClipboard
-
-modifyClipboard :: (String -> String) -> IO (Either String String)
-modifyClipboard = flip (liftM . liftM) getClipboard >=> either (return . Left) setClipboard
 
